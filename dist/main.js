6d3d107d2c195406dde7d83eccdcd1051732967e
FlareonTheProgrammer <flareon@notifier.link>
2021-04-14 12:55:14 -0500

    slight fix

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Database = void 0;
 const fs = require("fs");
 class Database {
     constructor(folder, name) {
